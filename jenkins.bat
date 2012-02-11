9c401b1fda72dde7a57c14ea4b7c25689d39697f
Dave Bethke <david.bethke@gmail.com>
2012-02-11 13:57:40 -0600

    checking postPush Script

---
+++ jenkins.bat
@@ -0,0 +1,3 @@
+cd C:\Program Files\Jenkins\jobs\IntegerSetUnitTests\workspace;
+del C:\Program Files\Jenkins\jobs\IntegerSetUnitTests\workspace\test_detail.xml ;
+"D:\Users\dave\Documents\Visual Studio 2010\Projects\IntegerSet\IntegerSet\debug\IntegerSet.exe"
