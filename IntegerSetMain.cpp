cfdff28ce97949f3cd09134b5192c5185d87235a
Dave Bethke <david.bethke@gmail.com>
2012-02-12 20:21:27 -0600

    interset test release and debug jenkins

--- IntegerSetMain.cpp
+++ IntegerSetMain.cpp
@@ -6,14 +6,15 @@
 #include "gtest/gtest.h"
 #include "IntegerSet.h"
 
+//TODO Jenkins Test Release and Debug build
 int main(int argc, char** argv) 
 {
 #ifdef _DEBUG
 		::testing::GTEST_FLAG(print_time) = true;
 		::testing::GTEST_FLAG(output) = "xml:C:\\Program Files\\Jenkins\\jobs\\IntegerSetUnitTests\\workspace\\test_detail.xml";
-	    testing::InitGoogleTest(&argc, argv); 
-	    RUN_ALL_TESTS(); 
-    //std::getchar(); // keep console window open until Return keystroke
+		testing::InitGoogleTest(&argc, argv); 
+		RUN_ALL_TESTS(); 
+	//std::getchar(); // keep console window open until Return keystroke
 #endif //_DEBUG
 	/*
 	IntegerSet iS;
---
+++ jenkinsP.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\Jenkins\jobs\IntegerSetRelease\workspace;
+del C:\Program Files\Jenkins\jobs\IntegerSetRelease\workspace\test_detail.xml ;
